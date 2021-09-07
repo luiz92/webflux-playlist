ed1ab80d97507c9a759012901bd76d33976de71e
José Luiz Alves <luizzs92@gmail.com>
2021-09-07 12:06:23 -0300

    Update

--- src/main/java/com/apirest/webfluxsample/service/PlaylistService.java
+++ src/main/java/com/apirest/webfluxsample/service/PlaylistService.java
@@ -1,11 +1,29 @@
 package com.apirest.webfluxsample.service;
 
 import com.apirest.webfluxsample.document.Playlist;
+import com.apirest.webfluxsample.repository.PlaylistRepository;
+import com.apirest.webfluxsample.request.PlaylistRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public interface PlaylistService {
-    Flux<Playlist> findAll();
-    Mono<Playlist>  finById(String id);
-    Mono<Playlist> save(Playlist playlist);
+@RequiredArgsConstructor
+@Service
+public class PlaylistService {
+
+    private final PlaylistRepository playlistRepository;
+
+    public Flux<Playlist> findAll() {
+        return playlistRepository.findAllByOrderByNome();
+    }
+
+    public Mono<Playlist> finById(String id) {
+        return playlistRepository.findById(id);
+    }
+
+    public Mono<Playlist> save(PlaylistRequest playlistRequest) {
+        var playlist = new Playlist(playlistRequest.getNome());
+        return playlistRepository.save(playlist);
+    }
 }
